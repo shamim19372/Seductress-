88ad82922482fd9ba4eabaa07cbf61fa6638322c
Xumit123 <kalikumari72727@gmail.com>
2024-11-27 11:59:28 +0530

    Create OWNER.js

---
+++ Priyansh/commands/OWNER.js
@@ -0,0 +1,29 @@
+ const fs = require("fs");
+module.exports.config = {
+	name: "owner",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("prefix") ||
+     react.includes("Virat") || 
+react.includes("owner")) {
+		var msg = {
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ fecebook https://www.facebook.com/profile.php?id=100030585286077`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
